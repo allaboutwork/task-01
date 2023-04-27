990648df78c275c2f0ec86e3eed41f1e95e7fba7
allaboutwork <afshanalamwork@outlook.com>
2023-04-27 15:13:28 +0530

    created migrations and models

--- app1/models.py
+++ app1/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class UserInstance(models.Model):
+    # fields for UserInstance table
+    pass
+
+class Client(models.Model):
+    name = models.CharField(max_length=100)
+    user_instance = models.ForeignKey(UserInstance, on_delete=models.CASCADE)
+
+class Artist(models.Model):
+    name = models.CharField(max_length=100)
+    work = models.ManyToManyField('Work')
+
+class Work(models.Model):
+    LINK_CHOICES = (
+        ('Youtube', 'Youtube'),
+        ('Instagram', 'Instagram'),
+        ('Other', 'Other')
+    )
+    link = models.URLField()
+    work_type = models.CharField(max_length=10, choices=LINK_CHOICES)
